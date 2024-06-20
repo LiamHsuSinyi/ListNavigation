95a4d30e11a1ff9566ce1bd10d496c92208d5d9e
LiamHsu <s459958@gmail.com>
2024-06-20 10:20:19 +0800

    Initial Commit

---
+++ ListNavigation/ListNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavigationApp.swift
+//  ListNavigation
+//
+//  Created by Liam on 2024/6/20.
+//
+
+import SwiftUI
+
+@main
+struct ListNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
