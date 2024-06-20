6c2632a8e624d41f029cfe0c22669d989d59cab5
LiamHsu <s459958@gmail.com>
2024-06-20 16:07:45 +0800

    Implement favorites only in landmark list

--- ListNavigation/Views/ContentView.swift
+++ ListNavigation/Views/ContentView.swift
@@ -9,18 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
-            List(ModelData.landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationTitle("Landmarks")
-        } detail: {
-            Text("Select a Landmark")
-        }
+        LandmarkList()
     }
 }
 
--- ListNavigation/Views/LandmarkList.swift
+++ ListNavigation/Views/LandmarkList.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct LandmarkList: View {
     
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        ModelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
-        
-        List(ModelData.landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
         }
     }
 }
